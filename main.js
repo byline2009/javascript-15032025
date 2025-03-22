133ded4ed919851d2f360d8fcf6feb2c8ffce28c
DiKang2 <duykhang149cm@gmail.com>
2025-03-22 10:01:42 +0700

    update baitap 2

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
-console.warn("Hello van anh");
-let text = "Hello, JavaScript!";
-console.log(Date()); // "HELLO, JAVASCRIPT!"
-function sayHello() {
-  alert("Hello, JavaScript!");
-}
+// let a = {
+//   name: "Duy Khang"
+// };
+// console.log(a)
+
+// let a ={
+// name:{
+
+//   firstname: "Duy",
+//   lastname: "Khang",
+
+// }
+
+
+// };
+
+// console.log(a)
+
+// // Nhap dai rong
+// let length = parseFloat(prompt("nhap chieu dai:"));
+// let width = parseFloat(prompt("nhap chieu rong:"));
+
+// if (length > 0 && width > 0) {
+//   let area = length * width;
+//   let perimeter = 2 * (length + width);
+
+//   // ket qua
+//   console.log(`Dien tich: ${area}`);
+//   console.log(`Chu vi: ${perimeter}`);
+// }else{
+//   console.log('Vui long nhap hop le')
+// }
+
+
+
+// ktra chan le
+//nhap so
+let number = parseInt(prompt("Nhap so nguyen:"));
+
+//dau ? dang if else
+let result = (number % 2 === 0) ? "số chẵn" : "số lẻ";
+
+// view
+console.log(`${number} là ${result}`);
+
+
