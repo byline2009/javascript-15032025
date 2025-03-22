e2cffd7911b082252eeb207fe6bbce6f56c9f674
TuanKiet <trantuankie192@gmail.com>
2025-03-22 12:06:11 +0700

    update

---
+++ ham.js
@@ -0,0 +1,15 @@
+let my = " i'm malenia blade of miquella "
+console.log(my.length) // chuỗi dài bao nhiêu
+
+console.log(my.indexOf('malenia')) //tìm vị trí của chuỗi
+
+console.log(my.slice(0,11)) // cắt chuỗi
+
+console.log(my.replace(/malenia/g , 'radan')) // thay thế chuỗi. /../g chọn toàn bộ ký tự  
+
+console.log(my.toUpperCase()) // tất các viết hoa
+//toLowerCase() // viết thường
+
+console.log(my.trim()) // loại bỏ khoảng trắng
+
+console.log(my.charAt(1)) // trả giá trị ở vị trí đó
