f8fa3250411f9d520ea108250cb063683ff14d5a
Admin <admin@MacBook-Pro-cua-Admin.local>
2025-04-06 10:49:36 +0700

    up josn and fetch

--- main.js
+++ main.js
@@ -1,29 +1,54 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Mã của bạn ở đây
-  let elementFromGroup = document.querySelector(`.form-group`);
-  if (elementFromGroup) {
-    let childFormControl = elementFromGroup.querySelector(".form-control");
-    let errorElement = elementFromGroup.querySelector(".form-message");
-    if (childFormControl) {
-      childFormControl.value = "Thai Vinh Le";
-      console.log(
-        "childFormControl.parentElement",
-        childFormControl.parentElement
-      );
-      childFormControl.onblur = () => {
-        console.log(
-          "childFormControl.value.length ",
-          childFormControl.value.length
-        );
-        if (childFormControl.value.length == 0) {
-          if (errorElement) {
-            errorElement.innerText = "Đây là trường bắt buộc nhập";
-          }
-        } else {
-          errorElement.innerText = "";
+  document
+    .getElementById("form")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault(); // Ngừng hành vi mặc định (submit form)
+      console.log("Form submission prevented");
+      const fullname = document.getElementById("fullname");
+      const email = document.getElementById("email");
+      const password = document.getElementById("password");
+      console.log("info", fullname.value, email.value, password.value);
+
+      const result = await fetch(
+        "https://ca5a80caf735f690b673.free.beeceptor.com/api/users?id=a63b0a6ff1143fcd54de",
+        {
+          method: "GET",
         }
-      };
-    }
-  }
-  console.log("elementFromGroups", elementFromGroup);
+      );
+
+      const data = await result.json();
+      console.log("data", data);
+
+      fetch(
+        "https://ca5a80caf735f690b673.free.beeceptor.com/api/users?id=a63b0a6ff1143fcd54de",
+        {
+          method: "GET",
+        }
+      )
+        .then(async (result) => await result.json())
+        .then((data) => {
+          console.log("data", data);
+        });
+
+      fetch("https://ca5a80caf735f690b673.free.beeceptor.com/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          fullname: fullname.value,
+          email: email.value,
+          password: password.value,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Success:", data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+
+  // Mã của bạn ở đây
 });
