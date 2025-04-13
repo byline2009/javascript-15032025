3e43c7d222593ad87f3c67c22094e1da12b90d39
Admin <admin@MacBook-Pro-cua-Admin.local>
2025-04-13 11:06:37 +0700

    up

--- main.js
+++ main.js
@@ -1,18 +1,77 @@
+const dataResult = [
+  {
+    fullname: "Duong",
+    email: "duong@gmail.com",
+  },
+  {
+    fullname: "Duy Khang",
+    email: "khang@gmail.com",
+  },
+  {
+    fullname: "Kiet",
+    email: "Kiet@gmail.com",
+  },
+];
+
 function Validator(options) {
-  document.addEventListener("DOMContentLoaded", function () {
+  const selectorRules = {};
+  document.addEventListener("DOMContentLoaded", async function () {
+    const tableBody = document.querySelector("#myTable tbody");
+    console.log("tableBody", tableBody);
+
+    const result = await fetch(
+      "https://ca5a80caf735f690b673.free.beeceptor.com/api/users"
+    );
+    const dataResult = await result.json();
+    dataResult.forEach((item, index) => {
+      const row = document.createElement("tr");
+
+      row.innerHTML = `
+          <td>${index + 1}</td>
+          <td>${item.fullname}</td>
+          <td>${item.email}</td>
+        `;
+
+      tableBody.appendChild(row);
+    });
     const formElement = document.querySelector(options.form);
-    formElement.onsubmit = function (e) {
+    formElement.onsubmit = async function (e) {
       e.preventDefault();
+      let isValid = undefined;
+      let data = {};
+      const inputElements = formElement.querySelectorAll("[name]");
+      // console.log("inputElements", inputElements);
+
+      inputElements.forEach((element) => {
+        // console.log("element.name", element.name, element);
+        isValid = validate(`#${element.name}`, element);
+        data[`${element.name}`] = element.value;
+        if (isValid) {
+          return;
+        }
+      });
+      if (!isValid) {
+        console.log("data from form", data);
+        const result = await fetch(
+          "https://ca5a80caf735f690b673.free.beeceptor.com/api/users",
+
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
+        console.log("result", result);
+      }
     };
-    const selectorRules = {};
     options.rules.forEach((rule) => {
       const element = document.querySelector(rule.selector);
       if (element) {
         // console.log("element", element);
         if (Array.isArray(selectorRules[rule.selector])) {
-          selectorRules[rule.selector] = selectorRules[rule.selector].push(
-            rule.test
-          );
+          selectorRules[rule.selector].push(rule.test);
         } else {
           selectorRules[rule.selector] = [rule.test];
         }
@@ -21,13 +80,32 @@ function Validator(options) {
       Array.from(inputElements).forEach(function (inputElement) {
         // Xử lý trường hợp blur khỏi input
         inputElement.onblur = function () {
-          console.log("onblur", inputElement.value);
+          // console.log("onblur", rule.selector);
+          validate(rule.selector, inputElement);
         };
       });
     });
-
     // console.log("selectorRules", selectorRules);
   });
+  function validate(selector, inputElement) {
+    let errorMessage = undefined;
+    const parentElement = inputElement.parentElement;
+    // console.log("parentElement", parentElement);
+    const messageElement = parentElement.querySelector(".form-message");
+    // console.log("messageElement", messageElement);
+    const rules = selectorRules[selector];
+    rules.forEach((rule, index) => {
+      errorMessage = rule(inputElement.value);
+      // console.log("errorMessage", errorMessage);
+      if (errorMessage) {
+        messageElement.innerText = errorMessage;
+      } else {
+        messageElement.innerText = "";
+      }
+    });
+
+    return errorMessage;
+  }
 }
 Validator.isRequired = function (selector, message) {
   return {
@@ -58,7 +136,21 @@ Validator.isEmail = function (selector, message) {
     selector: selector,
     test: (value) => {
       const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      if (regex.test(email)) {
+      if (regex.test(value)) {
+        return undefined;
+      } else {
+        return message;
+      }
+    },
+  };
+};
+
+Validator.isConfirmPassword = function (selector, message) {
+  return {
+    selector: selector,
+    test: (value) => {
+      const passwordElement = document.querySelector("#password");
+      if (passwordElement.value == value) {
         return undefined;
       } else {
         return message;
